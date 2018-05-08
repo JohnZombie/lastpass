33efaa19f9f3b5f82321d07fd81950a1180347e4
J0HN Z0MB1E <johnzombie1992@gmail.com>
2018-05-08 11:26:37 -0400

    Fixed Logic in script to check for LP

--- lastpasscheck.sh
+++ lastpasscheck.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 
-#This checks if lastpass plugin is installed for Chrome,Firefox, and on Computer.
+#Installs homebrew and lastpass CLI tools
 
-if [ ! -f "/Applications/LastPass.app" ]; then
-echo "Lastpass on Mac found."
-else
-echo "Lastpass on Mac not found Please download from Managed Software Center.."
-fi
-if [ ! -f "/Users/$USER/Library/Application\ Support/Google/Chrome/Default/Extensions/hdokiejnpimakedhajhdlcegeplioahd" ]; then
-echo "Lastpass on Chrome found."
-else
-echo "Lastpass on Chrome not found.. Please install from https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd?hl=en"
-fi
-if [ ! -f "/Users/$USER/Library/Application\ Support/Firefox/Profiles/hcbg6rr3.default-1472125718024/extensions/support@lastpass.com.xpi" ]; then
-echo "Lastpass on Firefox found."
-else
-echo "Lastpass on Firefox not found..Please install from https://addons.mozilla.org/en-US/firefox/addon/lastpass-password-manager/"
-fi
-# Installs homebrew and lastpass CLI tools
-function install_brew_osx() 
+function install_brew_osx()
 {
 echo "Now installing brew for OSX"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -28,7 +12,7 @@ brew install lastpass-cli --with-pinentry
 read -p "Please enter your lastpass email to login into lastpass: " USERNAME
 lpass login $USERNAME
 read -p "Would you like to see the contents in your vault (y/n)?" choice
-case "$choice" in 
+case "$choice" in
   y|Y ) lpass ls;;
   n|N ) echo "no";;
   * ) echo "invalid";;
@@ -37,4 +21,12 @@ echo "[*]Hit enter to see a list of commands for lastpass. To navigate you must 
 read
 lpass man
 }
+
+# Checks if Lastpass is installed on machine
+
+if [ -d  "/Users/$USER/Library/Containers/com.lastpass.LastPass" ]; then
+echo "Lastpass on Mac found."
 install_brew_osx
+else
+echo "Lastpass on Mac not found Please download from Managed Software Center.."
+fi
